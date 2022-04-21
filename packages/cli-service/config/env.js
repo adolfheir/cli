6dc7582b36ab0c5faa730bd6de4d42b8335fc00d
cyh <1428899467@qq.com>
2022-04-21 18:04:21 +0800

    fix: 版本信息

--- packages/cli-service/config/env.js
+++ packages/cli-service/config/env.js
@@ -2,8 +2,8 @@ const { get } = require("lodash");
 
 module.exports = {
   CI_BUILD_NUMBER: get(process.env, "CI_BUILD_NUMBER", 0),
-  GIT_BRANCH: get(process, "GIT_BRANCH", 0),
-  GIT_COMMIT_SHORT: get(process, "GIT_COMMIT_SHORT", 0),
+  GIT_BRANCH: get(process.env, "GIT_BRANCH", 0),
+  GIT_COMMIT_SHORT: get(process.env, "GIT_COMMIT_SHORT", 0),
   ENABLE_ANALYZER: get(process.env, "ENABLE_ANALYZER", 0),
   ENABLE_SOURCEMAP: get(process.env, "ENABLE_SOURCEMAP", 0),
 };
